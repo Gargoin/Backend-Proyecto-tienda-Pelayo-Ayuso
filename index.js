72306f684bfc61a2a5f46999160487230f3abb29
Pelayo Ayuso <pelayo.ayuso@gmail.com>
2026-06-04 19:30:58 +0200

    montado el server Express

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.json ({message: "Hola!"});
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`http//localhost:${PORT}`));
+
